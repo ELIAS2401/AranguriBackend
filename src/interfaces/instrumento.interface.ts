da13cac68036eb46104a36bda76ca27ed3d15e44
ELIAS2401 <tuccieliasluis@gmail.com>
2026-07-01 16:11:27 -0300

    Establecer estructura base, interfaces y manejo de errores

---
+++ src/interfaces/instrumento.interface.ts
@@ -0,0 +1,10 @@
+export interface IInstrumento {
+  id?: string;
+  nombre: string;
+  marca: string;
+  categoria: string;
+  precio: number;
+  descripcion: string;
+  imagen: string;
+  created_at?: string;
+}
